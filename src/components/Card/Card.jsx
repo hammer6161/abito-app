17b510d4dbc5e873140aa6a9b37d91a8db54b4af
Igor <96075714+hammer6161@users.noreply.github.com>
2025-10-02 19:55:11 +0300

    Add files via upload

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+import './Card.css'
+
+export const Card = ({id, title, price, address, date, image}) => {
+	
+  return (
+    <Link className="card" to={`/product/${id}`}>
+      <div className="card--img">
+        <img src={image} alt="card-img" />
+      </div>
+      <h5 className="card--title">{title}</h5>
+      <strong className="card--price">{price}</strong>
+      <div className="card--desc-box">
+        <span className="card--desc">
+          {address}
+        </span>
+        <span className="card--desc">{date}</span>
+      </div>
+    </Link>
+  );
+};
---
+++ src/pages/Home.jsx
@@ -0,0 +1,102 @@
+import { useOutletContext } from "react-router-dom";
+import { Card } from "../components/Card/Card";
+
+export const Home = () => {
+
+	const {products} = useOutletContext()
+		
+
+  return (
+    <>
+      
+        <section className="content">
+          <div className="container">
+            <div className="content-box">
+              <div className="content-main">
+                <h2 className="content-main__title">Рекомендации для вас</h2>
+                <div className="content-main__list">
+                  {products.map((card) => (
+                    <Card
+                      key={card.id}
+                      id={card.id}
+                      title={card.title}
+                      price={card.price}
+                      address={card.address}
+                      date={card.date}
+                      image={card.image}
+                    />
+                  ))}
+                </div>
+              </div>
+              <div className="content-side">
+                <h3 className="content-side__title">Сервисы и услуги</h3>
+
+                <div className="content-side__box">
+                  <div className="content-side__list">
+                    <div className="content-side__list-item">
+                      <img
+                        className="content-side__list-item--img"
+                        src="/image/delivery-truck.svg"
+                        alt="delivery-truck"
+                      />
+                      <h5 className="content-side__list-item--title">
+                        Доставка
+                      </h5>
+                      <p className="content-side__list-item--text">
+                        Проверка при получении и возможность бесплатно вернуть
+                        товар
+                      </p>
+                    </div>
+
+                    <div className="content-side__list-item">
+                      <img
+                        className="content-side__list-item--img"
+                        src="/image/sedan.svg"
+                        alt="sedan"
+                      />
+                      <h5 className="content-side__list-item--title">
+                        Автотека
+                      </h5>
+                      <p className="content-side__list-item--text">
+                        Отчёт с историей авто: пробег, владельцы, сведения о
+                        залоге, ДТП и ремонтах
+                      </p>
+                    </div>
+
+                    <div className="content-side__list-item">
+                      <img
+                        className="content-side__list-item--img"
+                        src="/image/love.svg"
+                        alt="love"
+                      />
+
+                      <h5 className="content-side__list-item--title">
+                        Онлайн-бронирование жилья
+                      </h5>
+                      <p className="content-side__list-item--text">
+                        Посуточная аренда квартир и домов: большой выбор
+                        вариантов для поездок по России
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="content-side__footer">
+                    <p className="content-side__footer--item">
+                      © ООО «Абито», 2011–2021
+                    </p>
+                    <a className="content-side__footer--item" href="#!">
+                      Политика конфиденциальности
+                    </a>
+                    <a className="content-side__footer--item" href="#!">
+                      Обработка данных
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+ 
+    </>
+  );
+};
